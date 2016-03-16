a3209e254c6eee44aed5c0bab47b17f389d75d31
bionic4655 <bykovpi@yandex.ru>
2016-03-16 08:41:44 +0500

    1
    new project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestNewsSite.Startup))]
+namespace TestNewsSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
